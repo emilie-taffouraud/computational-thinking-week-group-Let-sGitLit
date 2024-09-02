42f44c683c1ed2a315d343b22221aeeb5700bead
emilie-taffouraud <93034660+emilie-taffouraud@users.noreply.github.com>
2024-09-02 12:43:33 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -3,4 +3,4 @@ import emilie_print_name_bitch.py
 
 
 def team names(s,a,em,el,i):
-  print("This is Team YourTeamNameHere. We are:")
+  print("This is Team Let's Git Lit. We are:", emilie_name, sophia_name)
