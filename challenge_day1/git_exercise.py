88c45bb3a42f19e092452972181454c56e217eb8
lakaaa-38 <elnara.ibrahimli@student.uva.nl>
2024-09-06 12:24:12 +0200

    Update git_exercise.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,9 +1,13 @@
 import sophiasname, emilie_print_name, elnarochka, ireneblabla, annasname
 
-
-
 def team_names():
-  print("This is Team Let's Git Lit. We are:", emilie_name, sophia_name, elnara_name, anna_name, irene_name)
+  print("This is Team Let's Git Lit. We are: ")
+  
+  print(sophiasname.get_name())
+  print(emilie_print_name.get_name())
+  print(elnarochka.get_name())
+  print(ireneblabla.get_name())
+  print(annasname.get_name())
 
-
-print(team_names)
+if __name__ == "__main__":
+    team_introduction()
